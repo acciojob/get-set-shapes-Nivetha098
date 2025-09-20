fc9f6a4e549ae8a4f8e3963e98785b012bfdf782
Nivetha098 <45388667+Nivetha098@users.noreply.github.com>
2025-09-20 21:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ class Rectangle {
 	get height(){
 		return this._height;
 	}
+	getArea(){
+		return this._width * this._height;
+	}
 }
 
 class Square extends Animal {
@@ -20,6 +23,17 @@ class Square extends Animal {
 		return 4*this._side;
 	}
 }
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
+// Creating a Square instance
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
